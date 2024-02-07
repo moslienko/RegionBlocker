5231636fee31f37c9d5b62c4d4deb623ab6f448b
moslienko <8676976+moslienko@users.noreply.github.com>
2024-02-07 20:27:31 +0700

    Init example

---
+++ RegionBlockerExample/RegionBlockerExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RegionBlockerExample
+//
+//  Created by Pavel Moslienko on 07.02.2024.
+//
+
+import SwiftUI
+import RegionBlocker
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RegionBlockerExample/RegionBlockerExample/RegionBlockerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegionBlockerExampleApp.swift
+//  RegionBlockerExample
+//
+//  Created by Pavel Moslienko on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct RegionBlockerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
